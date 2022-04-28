3fa804fcfeb86f9bc7c005ac2940a8ebb1be8449
Tran Day <tran@cloverleaf.me>
2022-04-27 23:29:10 -0400

    created grid item template

--- utils/stringConstants.js
+++ utils/stringConstants.js
@@ -6,7 +6,11 @@ export const SIGN_UP = 'Sign up';
 export const MYLO_URL_TITLE = "Mylo - Your space in one place.";
 
 // Home 
-export const YOUR_PLACE = "Your space";
 export const ALL_IN_ONE_SPACE = "in one place.";
+export const ENABLING_TEAMS = "Enabling teams to do"
+export const SPEED_LESS_TIME = "Spend less time shuffling tabs with a comprehensive dashboard configuration and all your work tools at your fingertips."
+export const THEIR_BEST_WORK = "their best work"
 export const TRANSFORM_THE_WAY = "Transform the way you organize work, life and play with one place to get stuff done."
-
+export const WHAT_YOU_NEED = "What you need,"
+export const WHEN_YOU_NEED = "when you need it"
+export const YOUR_PLACE = "Your space";
