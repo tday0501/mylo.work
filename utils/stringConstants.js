ff750a192618e725b13d434f27e134d6a614bdd0
Tran M Day <tranmday@gmail.com>
2022-05-03 22:03:06 -0400

    writing website copy

--- utils/stringConstants.js
+++ utils/stringConstants.js
@@ -7,10 +7,14 @@ export const BACK_TO_HOME = "Back to home";
 export const MYLO_URL_TITLE = "Mylo - Your space in one place.";
 
 // Home
+export const BUILT_FOR = "Built for you, your team and everyday uses"
+export const CURATE_SPACE = "Curate your workspaces the way that works best for you and your teams."
+export const DONT_BE_BUSY = "Don't be busy be productive"
 export const ENABLING_TEAMS = "Enabling teams to do their best work";
+export const EASILY_SWITCH = "Easily switch between personal and professional life, jobs, tasks and contexts.";
 export const SPEED_LESS_TIME =
   "Spend less time shuffling tabs with a comprehensive dashboard configuration and all your work tools at your fingertips.";
 export const TRANSFORM_THE_WAY =
   "Transform the way you organize work, life and play with one place to get stuff done.";
-export const WHAT_YOU_NEED = "What you need, when you need it";
+export const WHATS_TRULY = "What's truly important all at your fingertip";
 export const YOUR_PLACE = "Your space in one place.";
