d7f179c0dca83ff9eaf0bfcf8ef01d79d5cfe31f
Tran M Day <tranmday@gmail.com>
2022-06-07 13:43:20 -0400

    implementing crisp

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import { ThemeProvider } from "@material-ui/core";
+import React from "react";
 import "../styles/global.css";
 import { theme } from "../utils/theme";
 
 function Mylo({ Component, pageProps }) {
+  React.useEffect(()=> {
+    window.$crisp=[];
+    window.CRISP_WEBSITE_ID="9ab2ab35-7b03-4622-8463-a51107a27a6b";
+    (function(){
+      d=document;
+      s=d.createElement("script");
+      s.src="https://client.crisp.chat/l.js";
+      s.async=1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+  });
+  
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
