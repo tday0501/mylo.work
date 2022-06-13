93bd4d47473456f7ab62f6c37605b126130a6864
Tran Day <tran@cloverleaf.me>
2022-06-13 08:54:27 -0400

    resize

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ export default function Home() {
         </Grid>
         <Grid item xs={12} sm={12} md={8} lg={8} justifyContent="flex-end">
           {imageSrc ? (
-            <Image alt={imageAlt} height={527} src={imageSrc} width={900} />
+            <Image alt={imageAlt} height={700} src={imageSrc} width={1100} />
           ) : (
             <Box>
               <video autoPlay playsInline loop muted className={classes.video}>
